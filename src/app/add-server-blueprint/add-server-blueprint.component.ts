72b071552333a2798d9c23adcb885b53ab519075
Aniket Gaikwad <aniketgaikwad9797@gmail.com>
2023-08-30 01:52:41 +0530

    emitting an event and custom event binding

--- src/app/add-server-blueprint/add-server-blueprint.component.ts
+++ src/app/add-server-blueprint/add-server-blueprint.component.ts
@@ -1,17 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-add-server-blueprint',
   templateUrl: './add-server-blueprint.component.html',
-  styleUrls: ['./add-server-blueprint.component.css']
+  styleUrls: ['./add-server-blueprint.component.css'],
 })
 export class AddServerBlueprintComponent implements OnInit {
   serverName = '';
   serverDesc = '';
-  constructor() { }
+  @Output() serverCreated = new EventEmitter<{}>();
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   onAddServer(serverType: string) {
     // this.serverList.push({
@@ -19,6 +19,10 @@ export class AddServerBlueprintComponent implements OnInit {
     //   description: this.serverDesc,
     //   type: serverType,
     // });
+    this.serverCreated.emit({
+      srvName: this.serverName,
+      srvDesc: this.serverDesc,
+      srcType: serverType
+    });
   }
-
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,9 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent implements OnInit {
-  serverList = [{ name: 'testServer', description: 'testDescription',type:'server' }];
+  serverList = [
+    { name: 'testServer', description: 'testDescription', type: 'server' },
+  ];
   //serverType = 'server';
   constructor() {}
 
   ngOnInit(): void {}
+
+  onServerCreated(serverInstance) {
+    console.log(serverInstance);
+    this.serverList.push({
+      name: serverInstance.srvName,
+      description: serverInstance.srvDesc,
+      type: serverInstance.srcType,
+    });
+    console.log(this.serverList)
+  }
 }
