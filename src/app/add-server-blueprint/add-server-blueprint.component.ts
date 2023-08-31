3750755763f38e14770dcf1ec41eed2243452d18
Aniket Gaikwad <aniketgaikwad9797@gmail.com>
2023-08-31 20:23:41 +0530

    Using Local reference instead of 2way data binding

--- src/app/add-server-blueprint/add-server-blueprint.component.ts
+++ src/app/add-server-blueprint/add-server-blueprint.component.ts
@@ -6,23 +6,27 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
   styleUrls: ['./add-server-blueprint.component.css'],
 })
 export class AddServerBlueprintComponent implements OnInit {
-  serverName = '';
-  serverDesc = '';
+  //serverName = '';
+  //serverDesc = '';
   @Output() serverCreated = new EventEmitter<{}>();
   constructor() {}
 
   ngOnInit(): void {}
 
-  onAddServer(serverType: string) {
+  onAddServer(
+    serverType: string,
+    serverName: HTMLInputElement,
+    sreverDescription: HTMLInputElement
+  ) {
     // this.serverList.push({
     //   name: this.serverName,
     //   description: this.serverDesc,
     //   type: serverType,
     // });
     this.serverCreated.emit({
-      srvName: this.serverName,
-      srvDesc: this.serverDesc,
-      srcType: serverType
+      srvName: serverName.value,
+      srvDesc: sreverDescription.value,
+      srcType: serverType,
     });
   }
 }
